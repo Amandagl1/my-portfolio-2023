8f75f401e6cf6633db95219ac70e25be843056a0
Amanda Lee <leeandama87@gmail.com>
2023-08-01 15:41:21 -0700

    Added a project to work section. Edited styling and colors of component.

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,15 +3,16 @@ import MoviesThatMove from '../assets/moviesthatmove.png'
 import Momentum from '../assets/momentum.png'
 import Mojave from '../assets/mojave.png'
 import JATE from '../assets/jate.png'
+import NoteTaker from '../assets/notetaker.png'
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen bg-[#F3D9E2]'>
+    <div name='work' className='w-full md:h-full bg-[#F3D9E2]'>
       <div className='
             max-w-[1000px] mx-auto w-full h-full p-4
             flex flex-col justify-center items-center'>
 
-        <div className='mt-4 inline justify-center text-center'>
+        <div className='mt-6 inline justify-center text-center'>
           <h2 className='
                  pb-4 text-center items-center
                 border-b-8 text-[#1C2439] border-[#9478D9]'>
@@ -27,12 +28,12 @@ const Work = () => {
             {/* Project card 1 */}
             <div style={{ backgroundImage: `url(${MoviesThatMove})` }}
               className='
-                mx-auto group container rounded-md
+                mx-auto group container border-y-8 border-[#1C2439] rounded-md
                 flex justify-center items-center work-content
-                shadow-lg shadow-white'>
+                shadow-lg shadow-[#f7d48f]'>
 
               {/* When user hovers over projects */}
-              <div className='opacity-0 group-hover:opacity-100 duration-300'>
+              <div className='opacity-0 group-hover:opacity-100'>
                 <span className='font-[Poppins] text-4xl text-white tracking-wider'>
 
                   Movies That Move
@@ -53,11 +54,11 @@ const Work = () => {
             {/* Project card 2 */}
             <div style={{ backgroundImage: `url(${Momentum})` }}
               className='
-                mx-auto group container rounded-md
+                mx-auto group container border-y-8 border-[#1C2439] rounded-md
                 flex justify-center items-center work-content
-                shadow-lg shadow-white'>
+                shadow-lg shadow-[#f7d48f]'>
 
-              <div className='opacity-0 group-hover:opacity-100 duration-300'>
+              <div className='opacity-0 group-hover:opacity-100'>
                 <span className='font-[Poppins] text-4xl text-white tracking-wider'>
 
                   Momentum
@@ -80,11 +81,11 @@ const Work = () => {
             {/* Project card 3 */}
             <div style={{ backgroundImage: `url(${Mojave})` }}
               className='
-                mx-auto group container rounded-md
+                mx-auto group container border-y-8 border-[#1C2439] rounded-md
                 flex justify-center items-center work-content
-                shadow-lg shadow-white'>
+                shadow-lg shadow-[#f7d48f]'>
 
-              <div className='opacity-0 group-hover:opacity-100 duration-300'>
+              <div className='opacity-0 group-hover:opacity-100'>
                 <span className='font-[Poppins] text-4xl text-white tracking-wider'>
 
                   Mojave
@@ -106,11 +107,11 @@ const Work = () => {
             {/* Project card 4 */}
             <div style={{ backgroundImage: `url(${JATE})` }}
               className='
-                mx-auto group container rounded-md
+                mx-auto group container border-y-8 border-[#1C2439] rounded-md
                 flex justify-center items-center work-content
-                shadow-lg shadow-white'>
+                shadow-lg shadow-[#f7d48f]'>
 
-              <div className='opacity-0 group-hover:opacity-100 duration-300'>
+              <div className='opacity-0 group-hover:opacity-100'>
                 <span className='font-[Poppins] text-4xl text-white tracking-wider'>
 
                   JATE
@@ -128,8 +129,34 @@ const Work = () => {
                 </div>
               </div>
             </div>
-          </div>
 
+            {/* Project card 5 */}
+            <div style={{ backgroundImage: `url(${NoteTaker})` }}
+              className='
+                mx-auto group container border-y-8 border-[#1C2439] rounded-md
+                flex justify-center items-center work-content
+                shadow-lg shadow-[#f7d48f]'>
+
+              <div className='opacity-0 group-hover:opacity-100'>
+                <span className='font-[Poppins] text-4xl text-white tracking-wider'>
+
+                  Note Taker
+
+                </span>
+                <div className='pt-8 text-center'>
+                  <a href="https://note-taker-2023.herokuapp.com/">
+                    <button className='
+                            font-[Lobster] m-2 px-6 py-2 rounded-lg 
+                            text-2xl text-center text-white bg-[#9478D9]'>
+                      View
+                    </button>
+                  </a>
+                  
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
